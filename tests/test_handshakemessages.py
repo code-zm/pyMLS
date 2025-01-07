23c423c6b4fd7c84334a48765ba2dffec607c37b
zmain4 <zmain4@proton.me>
2025-01-07 15:56:05 -0600

    updated tests

--- tests/test_handshakemessages.py
+++ tests/test_handshakemessages.py
@@ -1,56 +1,73 @@
 import unittest
+import os
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
-from pyMLS.HandshakeMessages import Add, Update, Remove, Commit, HandshakeMessage
+from pyMLS.HandshakeMessages import HandshakeMessage, HandshakeType
 from pyMLS.KeyPackage import KeyPackage
-from pyMLS.HandshakeTypes import HandshakeType
 
 class TestHandshakeMessages(unittest.TestCase):
 
     def setUp(self):
-        self.key_package = KeyPackage(
-            version=1,
-            cipherSuite=1,
-            initKey=b'\x01' * 32,
-            leafNode={"public_key": "dummy"},
-            extensions=[],
-        )
+        # Generate keys
         self.private_key = Ed25519PrivateKey.generate()
         self.public_key = self.private_key.public_key()
 
-        self.proposals = [Add(self.key_package)]
-        self.commit_msg = Commit(
-            proposals=self.proposals,
-            commitSecret=b'\x02' * 32
+        # Use bytes for credentials and extensions
+        credential = b"test_credential"
+        extensions = [b"test_extension_1", b"test_extension_2"]
+
+        # Initialize KeyPackage
+        self.key_package = KeyPackage(
+            version=1,
+            cipherSuite=1,
+            initKey=os.urandom(32),
+            leafNode={
+                "leafNodeSource": "test_leaf_node",
+                "signatureKey": os.urandom(32),
+            },
+            extensions=extensions,
+            credential=credential,
+            signature=None,
         )
 
-    def test_add_serialize_deserialize(self):
-        add_msg = Add(self.key_package)
+
+    def test_handshake_serialize_deserialize(self):
+        # Test for ADD
+        add_msg = HandshakeMessage(HandshakeType.ADD, b"payload_add")
         serialized = add_msg.serializeBinary()
-        deserialized = Add.deserializeBinary(serialized)
-        self.assertEqual(self.key_package.serialize(), deserialized.keyPackage.serialize())
+        deserialized = HandshakeMessage.deserializeBinary(serialized)
+        self.assertEqual(add_msg.messageType, deserialized.messageType)
+        self.assertEqual(add_msg.payload, deserialized.payload)
 
-    def test_update_serialize_deserialize(self):
-        update_msg = Update(self.key_package)
+        # Test for UPDATE
+        update_msg = HandshakeMessage(HandshakeType.UPDATE, b"payload_update")
         serialized = update_msg.serializeBinary()
-        deserialized = Update.deserializeBinary(serialized)
-        self.assertEqual(self.key_package.serialize(), deserialized.keyPackage.serialize())
+        deserialized = HandshakeMessage.deserializeBinary(serialized)
+        self.assertEqual(update_msg.messageType, deserialized.messageType)
+        self.assertEqual(update_msg.payload, deserialized.payload)
 
-    def test_remove_serialize_deserialize(self):
-        remove_msg = Remove(42)
+        # Test for REMOVE
+        remove_msg = HandshakeMessage(HandshakeType.REMOVE, b"payload_remove")
         serialized = remove_msg.serializeBinary()
-        deserialized = Remove.deserializeBinary(serialized)
-        self.assertEqual(remove_msg.removedIndex, deserialized.removedIndex)
+        deserialized = HandshakeMessage.deserializeBinary(serialized)
+        self.assertEqual(remove_msg.messageType, deserialized.messageType)
+        self.assertEqual(remove_msg.payload, deserialized.payload)
 
-    def test_commit_serialize_deserialize(self):
-        serialized = self.commit_msg.serializeBinary()
-        deserialized = Commit.deserializeBinary(serialized)
-        self.assertEqual(self.commit_msg.commitSecret, deserialized.commitSecret)
-        self.assertEqual(len(self.commit_msg.proposals), len(deserialized.proposals))
+    def test_signature_validation(self):
+        # Create and sign a message
+        msg = HandshakeMessage(HandshakeType.COMMIT, b"commit_payload")
+        signature = self.private_key.sign(msg.serializeBinary())
+        
+        # Validate signature
+        self.public_key.verify(signature, msg.serializeBinary())
 
-    def test_commit_sign_verify(self):
-        self.commit_msg.sign(self.private_key)
-        self.assertTrue(self.commit_msg.signature is not None)
-        self.assertTrue(self.commit_msg.verify(self.public_key))
+        # Test invalid signature
+        with self.assertRaises(Exception):
+            self.public_key.verify(b"invalid_signature", msg.serializeBinary())
+
+    def test_invalid_deserialization(self):
+        # Test malformed input
+        with self.assertRaises(Exception):
+            HandshakeMessage.deserializeBinary(b"malformed_input")
 
 if __name__ == "__main__":
     unittest.main()
